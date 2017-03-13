7d4c9c4a7819971873878aa76e252e9a33b5f8a8
Joey Lee <jleepianist@gmail.com>
2017-03-13 12:32:27 -0700

    (tests) added unit tests for index.js's Board class

--- test/test.js
+++ test/test.js
@@ -1,10 +1,24 @@
 var assert = require('assert');
 var Board = require('../index.js')
 
-describe('Tictactoe', function() {
+describe('Tictactoe board', function() {
+	var myBoard = new Board();
+
   describe('new Board', function() {
     it('should return a new empty board', function() {
-      assert.deepEqual([[],[],[]], (new Board()).board);
+      assert.deepEqual([[],[],[]], myBoard.board);
     });
+  });
+
+  describe('makeMove method', function() {
+  	it ('should allow each player to make moves', function() {
+  		assert.equal();
+  	})
+  });
+
+  describe('checkWinStatus method', function() {
+  	it ('should check after a winning move and announce which player won', function() {
+  		assert.equal();
+  	})
   });
 });
